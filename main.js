33b4b728e88896122a468c4280f474cd2bca2871
th3c0d3rush <th3co0d3rush@protonmail.com>
2021-02-20 10:10:08 +0100

    hamburger menu animation done and start open hamburger menu slide in

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const hamburgerMenu = document.querySelector('.Header__StyleHamburgerMenu');
+const Menu = document.querySelector('.Menu');
+
 let isMenuOpen = false;
 hamburgerMenu.addEventListener('click', () => {
   if(!isMenuOpen) {
     hamburgerMenu.classList.add('open');
     isMenuOpen = true;    
+    Menu.classList.add('open');
   } else {
     hamburgerMenu.classList.remove('open');
     isMenuOpen = false;
+    Menu.classList.remove('open');
   }
 });
